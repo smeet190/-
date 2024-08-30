46908fa395fcebdcd0fd16b9db0c0a27df819150
smeet190 <68428365+smeet190@users.noreply.github.com>
2024-08-30 18:04:10 +0530

    Update Kala.py

--- Kala.py
+++ Kala.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7400337646:AAG58cKuTq4gJcp0Q7n7sfPFJt9yW5cpsAI'
-ADMIN_ID = '6862918172'
+API_TOKEN = '7516697256:AAFvtsSEeK0VNliBSIdMaKnkOe9t8sIFu8I'
+ADMIN_ID = '6098218079'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
